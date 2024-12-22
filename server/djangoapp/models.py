f7e6e01766458c0b42ff8c9ab135652970713928
NyuJeongSheng <nyujsheng@gmail.com>
2024-12-22 03:54:03 -0500

    Refractor codes in models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -16,7 +16,7 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
-    
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -42,11 +42,11 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-            validators=[
-                MaxValueValidator(2023),
-                MinValueValidator(2015)
-            ])
+                validators=[
+                    MaxValueValidator(2023),
+                    MinValueValidator(2015)
+                ])
     # Other fields as needed
-    
+
     def __str__(self):
         return self.name  # Return the name as the string representation
