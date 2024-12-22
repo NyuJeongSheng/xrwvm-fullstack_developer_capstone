88486b8384fe50f95a1c65137c41e172b192b5c1
NyuJeongSheng <nyujsheng@gmail.com>
2024-12-22 05:32:54 -0500

    Created main.yml

--- server/database/app.js
+++ server/database/app.js
@@ -82,7 +82,6 @@ app.get('/fetchDealers/:state', async (req, res) => {
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-    //Write your code here
     try {
         const documents = await Dealerships.find({id: req.params.id});
         res.json(documents);
